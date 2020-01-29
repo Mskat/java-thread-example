ff2fac946730d6b93fd5cd046b4c14301569c3a5
unknown <zur.katarzyna@gmail.com>
2020-01-29 22:20:38 +0000

    Create Counter class with Runnable implementation
    Created main class. Used join() method to make sure that child threads will end before the end of the main thread.
    Created nested class named Counter with Runnable implementation and constructor.

--- src/main/java/Countdown.java
+++ src/main/java/Countdown.java
@@ -1,5 +1,39 @@
 public class Countdown {
 
+    public static void main(String[] args) {
+        Person person = new Person();
+        Counter firstPerson = new Counter(person, "Hana");
+        Counter secondPerson = new Counter(person, "Pola");
+        Counter thirdPerson = new Counter(person, "Max");
+
+        try {
+            firstPerson.thread.join();
+            secondPerson.thread.join();
+            thirdPerson.thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Counter implements Runnable{
+
+    private Person person;
+    private String personName;
+    Thread thread;
+
+    public Counter(Person person, String personName) {
+        this.person = person;
+        this.personName = personName;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        person.personCountsDown(personName);
+    }
+
 }
 
 class Person {
